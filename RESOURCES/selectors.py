ae8fee5161b8d4fb5d9dd9532b3e6b7b53793ca2
jasminzip <jasminpalao@gmail.com>
2025-10-14 21:04:32 +0800

    Update selectors.py

--- RESOURCES/selectors.py
+++ RESOURCES/selectors.py
@@ -1,4 +1,10 @@
 SELECTORS = {
+    # Login Page
     "username_input": "id=user-name",
     "password_input": "id=password",
-    "login_button": "id=login-button"
+    "login_button": "id=login-button",
+
+    # Logout Page / Menu
+    "menu_button": "id=react-burger-menu-btn",
+    "logout_link": "id=logout_sidebar_link"
+}
